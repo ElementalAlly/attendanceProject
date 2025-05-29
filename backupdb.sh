9f20089725e008327a8e13dfff966a99e242118d
Ethan Djajadi <ethan.djajadi@gmail.com>
2025-05-29 15:50:43 -0400

    Remove dump date

--- backupdb.sh
+++ backupdb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git -C /app/$1 pull
-mysqldump --add-drop-database --user=root --password="$2" --result-file="/app/$1/backupdb.sql" --databases attendancedb
+mysqldump --add-drop-database --skip-dump-date --user=root --password="$2" --result-file="/app/$1/backupdb.sql" --databases attendancedb
 git -C /app/$1 commit -am "update"
 git -C /app/$1 push
